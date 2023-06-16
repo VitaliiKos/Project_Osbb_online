c5dab33648faf22f3bb64f6cdb38a79aa4dbb2d2
VitaliiKosyk <kostamtik@gmail.com>
2023-06-16 21:49:24 +0300

    docker nginx

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,11 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:4200",
-]
+ALLOWED_HOSTS = ['*']
+
 AUTH_USER_MODEL = 'users.UserModel'
 
 # Application definition
@@ -44,7 +41,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
-    'corsheaders',
 
     # my_apps
     'core',
@@ -61,7 +57,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
